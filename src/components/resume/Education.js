c97776e6c8b42d54687bb0595ae78317234adc80
govind <govind@example.com>
2023-09-18 00:18:08 +0530

    Initial Commit

--- src/components/resume/Education.js
+++ src/components/resume/Education.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { motion } from 'framer-motion';
 import ResumeCard from './ResumeCard';
 
@@ -12,26 +12,26 @@ const Education = () => {
       {/* part one */}
       <div>
         <div className="py-6 lgl:py-12 font-titleFont flex flex-col gap-4">
-          <p className="text-sm text-designColor tracking-[4px]">1998 - 2010</p>
+          <p className="text-sm text-designColor tracking-[4px]">1998 - 2014</p>
           <h2 className="text-3xl md:text-4xl font-bold">Education Quality</h2>
         </div>
         <div className="mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
           <ResumeCard
-            title="BSc in Computer Science"
-            subTitle="University of DVI (2006 - 2010)"
-            result="3.90/4"
+            title="Master's in Information Technology"
+            subTitle="University of Mumbai (2011 - 2014)"
+            result="First"
             des="The training provided by universities in order to prepare people to work in various sectors of the economy or areas of culture."
           />
           <ResumeCard
-            title="AS - Science & Information"
-            subTitle="SuperKing College (2001 - 2005)"
-            result="4.75/5"
+            title="BE in Information Technology"
+            subTitle="Dr. B.A.M.University"
+            result="First"
             des="Higher education is tertiary education leading to award of an academic degree. Higher education, also called post-secondary education."
           />
           <ResumeCard
-            title="Secondary School Education"
-            subTitle="Kingstar Secondary School (1998 - 2000)"
-            result="5.00/5"
+            title="Higher Secondary School Education"
+            subTitle="S.B.H.School"
+            result="First"
             des="Secondary education or post-primary education covers two phases on the International Standard Classification of Education scale."
           />
         </div>
@@ -40,32 +40,32 @@ const Education = () => {
 
       <div>
         <div className="py-6 lgl:py-12 font-titleFont flex flex-col gap-4">
-          <p className="text-sm text-designColor tracking-[4px]">2010 - 2022</p>
+          <p className="text-sm text-designColor tracking-[4px]">2014 - 2023</p>
           <h2 className="text-3xl md:text-4xl font-bold">Job Experience</h2>
         </div>
         <div className="mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
           <ResumeCard
             title="Sr. Software Engineer"
-            subTitle="Google Out Tech - (2017 - Present)"
-            result="USA"
-            des="Google's hiring process is an important part of our culture. Googlers care deeply about their teams and the people who make them up."
+            subTitle="2021-present"
+            result="Pune"
+            des="MERN Full Stack Developer."
           />
           <ResumeCard
-            title="Web Developer & Trainer"
-            subTitle="Apple Developer Team - (2012 - 2016)"
-            result="MALAYSIA"
+            title="Web Developer"
+            subTitle="Pune - (2014 - 2020)"
+            result="Pune"
             des="A popular destination with a growing number of highly qualified homegrown graduates, it's true that securing a role in Malaysia isn't easy."
           />
           <ResumeCard
             title="Front-end Developer"
-            subTitle="Nike - (2020 - 2011)"
-            result="Oman"
-            des="The Oman economy has grown strongly over recent years, having transformed itself from a producer and innovation-based economy."
+            subTitle="Cynapse India Pvt. Ltd.- (2007 - 2007)"
+            result="Mumbai"
+            des="The Cynapse has grown strongly over recent years, having transformed itself from a producer and innovation-based economy."
           />
         </div>
       </div>
     </motion.div>
   );
-}
+};
 
-export default Education
+export default Education;
--- src/components/tesimonial/Testimonial.js
+++ src/components/tesimonial/Testimonial.js
@@ -1,10 +1,9 @@
-import React,{useState} from 'react'
-import Slider from "react-slick";
-import { RiStarFill } from "react-icons/ri";
-import { HiArrowRight, HiArrowLeft } from "react-icons/hi";
-import Title from '../layouts/Title'
-import { testimonialOne,testimonialTwo, quote } from "../../assets";
-
+import React, { useState } from 'react';
+import Slider from 'react-slick';
+import { RiStarFill } from 'react-icons/ri';
+import { HiArrowRight, HiArrowLeft } from 'react-icons/hi';
+import Title from '../layouts/Title';
+import { testimonialOne, testimonialTwo, quote } from '../../assets';
 
 function SampleNextArrow(props) {
   const { onClick } = props;
@@ -31,62 +30,62 @@ function SamplePrevArrow(props) {
 }
 
 const Testimonial = () => {
-      const [dotActive, setDocActive] = useState(0);
-     const settings = {
-       dots: true,
-       infinite: true,
-       speed: 500,
-       slidesToShow: 1,
-       slidesToScroll: 1,
-       nextArrow:<SampleNextArrow />,
-       prevArrow:<SamplePrevArrow />,
-       beforeChange: (prev, next) => {
-         setDocActive(next);
-       },
-       appendDots: (dots) => (
-         <div
-           style={{
-             borderRadius: "10px",
-             padding: "10px",
-           }}
-         >
-           <ul
-             style={{
-               display: "flex",
-               gap: "15px",
-               justifyContent: "center",
-               marginTop: "20px",
-             }}
-           >
-             {" "}
-             {dots}{" "}
-           </ul>
-         </div>
-       ),
-       customPaging: (i) => (
-         <div
-           style={
-             i === dotActive
-               ? {
-                   width: "12px",
-                   height: "12px",
-                   color: "blue",
-                   background: "#ff014f",
-                   borderRadius: "50%",
-                   cursor: "pointer",
-                 }
-               : {
-                   width: "12px",
-                   height: "12px",
-                   color: "blue",
-                   background: "gray",
-                   borderRadius: "50%",
-                   cursor: "pointer",
-                 }
-           }
-         ></div>
-       ),
-     };
+  const [dotActive, setDocActive] = useState(0);
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    beforeChange: (prev, next) => {
+      setDocActive(next);
+    },
+    appendDots: (dots) => (
+      <div
+        style={{
+          borderRadius: '10px',
+          padding: '10px',
+        }}
+      >
+        <ul
+          style={{
+            display: 'flex',
+            gap: '15px',
+            justifyContent: 'center',
+            marginTop: '20px',
+          }}
+        >
+          {' '}
+          {dots}{' '}
+        </ul>
+      </div>
+    ),
+    customPaging: (i) => (
+      <div
+        style={
+          i === dotActive
+            ? {
+                width: '12px',
+                height: '12px',
+                color: 'blue',
+                background: '#ff014f',
+                borderRadius: '50%',
+                cursor: 'pointer',
+              }
+            : {
+                width: '12px',
+                height: '12px',
+                color: 'blue',
+                background: 'gray',
+                borderRadius: '50%',
+                cursor: 'pointer',
+              }
+        }
+      ></div>
+    ),
+  };
   return (
     <section
       id="testimonial"
@@ -110,9 +109,9 @@ const Testimonial = () => {
                   <p className="text-xs uppercase text-designColor tracking-wide mb-2">
                     Bound - Trolola
                   </p>
-                  <h3 className="text-2xl font-bold">Jone Duone Joe</h3>
+                  <h3 className="text-2xl font-bold">Govind Wakure</h3>
                   <p className="text-base tracking-wide text-gray-500">
-                    Operation Officer
+                    MERN Full Stack Developer
                   </p>
                 </div>
               </div>
@@ -253,6 +252,6 @@ const Testimonial = () => {
       </div>
     </section>
   );
-}
+};
 
-export default Testimonial
+export default Testimonial;
